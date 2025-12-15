a7aa98b6e7c487bead624edf3042d9501ff8d3bf
kyxsanentah-glitch <kyxsanentah@gmail.com>
2025-12-15 20:55:24 +0700

    Add files via upload

---
+++ main/mbut12.sh
@@ -0,0 +1,245 @@
+#!/bin/bash
+
+# Target file tampilan admin
+REMOTE_PATH="/var/www/pterodactyl/resources/views/layouts/admin.blade.php"
+TIMESTAMP=$(date -u +"%Y-%m-%d-%H-%M-%S")
+BACKUP_PATH="${REMOTE_PATH}.bak_${TIMESTAMP}"
+
+echo "🚀 Memasang proteksi Tampilan Sidebar (Menu Hilang)..."
+
+# 1. Backup file lama
+if [ -f "$REMOTE_PATH" ]; then
+  mv "$REMOTE_PATH" "$BACKUP_PATH"
+  echo "📦 Backup file lama aman di: $BACKUP_PATH"
+fi
+
+# 2. Buat ulang file admin.blade.php dengan logika IF ID=1
+# PENTING: Menggunakan cat << 'EOF' agar HTML tidak dianggap command bash
+cat > "$REMOTE_PATH" << 'EOF'
+{{-- Pterodactyl - Panel --}}
+{{-- Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com> --}}
+
+{{-- This software is licensed under the terms of the MIT license. --}}
+{{-- https://opensource.org/licenses/MIT --}}
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>{{ config('app.name', 'Pterodactyl') }} - @yield('title')</title>
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <meta name="_token" content="{{ csrf_token() }}">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="/favicons/manifest.json">
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
+        <link rel="shortcut icon" href="/favicons/favicon.ico">
+        <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+        <meta name="theme-color" content="#0e4688">
+
+        @include('layouts.scripts')
+
+        @section('scripts')
+            {!! Theme::css('vendor/select2/select2.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/adminlte/adminlte.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/adminlte/skin-blue.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/sweetalert/sweetalert.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/animate/animate.min.css?t={cache-version}') !!}
+            {!! Theme::css('css/pterodactyl.css?t={cache-version}') !!}
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+
+            @show
+    </head>
+    <body class="hold-transition skin-blue fixed sidebar-mini">
+        <div class="wrapper">
+            <header class="main-header">
+                <a href="{{ route('index') }}" class="logo">
+                    <span>{{ config('app.name', 'Pterodactyl') }}</span>
+                </a>
+                <nav class="navbar navbar-static-top">
+                    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <div class="navbar-custom-menu">
+                        <ul class="nav navbar-nav">
+                            <li class="user-menu">
+                                <a href="{{ route('account') }}">
+                                    <img src="https://www.gravatar.com/avatar/{{ md5(strtolower(Auth::user()->email)) }}?s=160" class="user-image" alt="User Image">
+                                    <span class="hidden-xs">{{ Auth::user()->name_first }} {{ Auth::user()->name_last }}</span>
+                                </a>
+                            </li>
+                            <li>
+                                <li><a href="{{ route('index') }}" data-toggle="tooltip" data-placement="bottom" title="Exit Admin Control"><i class="fa fa-server"></i></a></li>
+                            </li>
+                            <li>
+                                <li><a href="{{ route('auth.logout') }}" id="logoutButton" data-toggle="tooltip" data-placement="bottom" title="Logout"><i class="fa fa-sign-out"></i></a></li>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+            <aside class="main-sidebar">
+                <section class="sidebar">
+                    <ul class="sidebar-menu">
+                        <li class="header">BASIC ADMINISTRATION</li>
+                        <li class="{{ Route::currentRouteName() === 'admin.index' ? 'active' : '' }}">
+                            <a href="{{ route('admin.index') }}">
+                                <i class="fa fa-home"></i> <span>Overview</span>
+                            </a>
+                        </li>
+                        
+                        {{-- HANYA ID 1: SETTINGS --}}
+                        @if(Auth::user()->id == 1)
+                        <li class="{{ Route::currentRouteName() === 'admin.settings' ? 'active' : '' }}">
+                            <a href="{{ route('admin.settings') }}">
+                                <i class="fa fa-wrench"></i> <span>Settings</span>
+                            </a>
+                        </li>
+                        @endif
+
+                        <li class="{{ Route::currentRouteName() === 'admin.api.index' ? 'active' : '' }}">
+                            <a href="{{ route('admin.api.index') }}">
+                                <i class="fa fa-gamepad"></i> <span>Application API</span>
+                            </a>
+                        </li>
+                        <li class="header">MANAGEMENT</li>
+                        <li class="{{ Route::currentRouteName() === 'admin.databases' ? 'active' : '' }}">
+                            <a href="{{ route('admin.databases') }}">
+                                <i class="fa fa-database"></i> <span>Databases</span>
+                            </a>
+                        </li>
+                        
+                        {{-- HANYA ID 1: LOCATIONS & NODES --}}
+                        @if(Auth::user()->id == 1)
+                        <li class="{{ Route::currentRouteName() === 'admin.locations' ? 'active' : '' }}">
+                            <a href="{{ route('admin.locations') }}">
+                                <i class="fa fa-globe"></i> <span>Locations</span>
+                            </a>
+                        </li>
+                        <li class="{{ Route::currentRouteName() === 'admin.nodes' ? 'active' : '' }}">
+                            <a href="{{ route('admin.nodes') }}">
+                                <i class="fa fa-sitemap"></i> <span>Nodes</span>
+                            </a>
+                        </li>
+                        @endif
+
+                        <li class="{{ Route::currentRouteName() === 'admin.servers' ? 'active' : '' }}">
+                            <a href="{{ route('admin.servers') }}">
+                                <i class="fa fa-server"></i> <span>Servers</span>
+                            </a>
+                        </li>
+                        <li class="{{ Route::currentRouteName() === 'admin.users' ? 'active' : '' }}">
+                            <a href="{{ route('admin.users') }}">
+                                <i class="fa fa-users"></i> <span>Users</span>
+                            </a>
+                        </li>
+                        <li class="header">SERVICE MANAGEMENT</li>
+                        
+                        {{-- HANYA ID 1: MOUNTS & NESTS --}}
+                        @if(Auth::user()->id == 1)
+                            <li class="{{ Route::currentRouteName() === 'admin.mounts' ? 'active' : '' }}">
+                                <a href="{{ route('admin.mounts') }}">
+                                    <i class="fa fa-magic"></i> <span>Mounts</span>
+                                </a>
+                            </li>
+                            <li class="{{ Route::currentRouteName() === 'admin.nests' ? 'active' : '' }}">
+                                <a href="{{ route('admin.nests') }}">
+                                    <i class="fa fa-th-large"></i> <span>Nests</span>
+                                </a>
+                            </li>
+                        @endif
+                        
+                    </ul>
+                </section>
+            </aside>
+            <div class="content-wrapper">
+                <section class="content-header">
+                    @yield('content-header')
+                </section>
+                <section class="content">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            @if (count($errors) > 0)
+                                <div class="alert alert-danger">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <strong>Error!</strong><br><br>
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+                            @foreach (Alert::getMessages() as $type => $messages)
+                                @foreach ($messages as $message)
+                                    <div class="alert alert-{{ $type }} alert-dismissable" role="alert">
+                                        {!! $message !!}
+                                    </div>
+                                @endforeach
+                            @endforeach
+                        </div>
+                    </div>
+                    @yield('content')
+                </section>
+            </div>
+            <footer class="main-footer">
+                <div class="pull-right hidden-xs">
+                    <b>Version</b> {{ config('app.version') }}
+                </div>
+                <strong>Copyright &copy; 2015 - {{ date('Y') }} <a href="https://pterodactyl.io/">Pterodactyl Software</a>.</strong>
+            </footer>
+        </div>
+        @section('footer-scripts')
+            <script src="/js/keyboard.polyfill.js" type="application/javascript"></script>
+            <script>keyboardeventKeyPolyfill.polyfill();</script>
+
+            {!! Theme::js('vendor/jquery/jquery.min.js?t={cache-version}') !!}
+            {!! Theme::js('vendor/sweetalert/sweetalert.min.js?t={cache-version}') !!}
+            {!! Theme::js('vendor/bootstrap/bootstrap.min.js?t={cache-version}') !!}
+            {!! Theme::js('vendor/slimscroll/jquery.slimscroll.min.js?t={cache-version}') !!}
+            {!! Theme::js('vendor/adminlte/app.min.js?t={cache-version}') !!}
+            {!! Theme::js('vendor/bootstrap-notify/bootstrap-notify.min.js?t={cache-version}') !!}
+            {!! Theme::js('vendor/select2/select2.full.min.js?t={cache-version}') !!}
+            {!! Theme::js('js/admin/functions.js?t={cache-version}') !!}
+            <script src="/js/autocomplete.js" type="application/javascript"></script>
+
+            @if(Auth::user()->root_admin)
+                <script>
+                    $('#logoutButton').on('click', function (event) {
+                        event.preventDefault();
+                        var that = this;
+                        swal({
+                            title: 'Do you want to log out?',
+                            type: 'warning',
+                            showCancelButton: true,
+                            confirmButtonColor: '#d9534f',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: 'Log out'
+                        }, function () {
+                            window.location = $(that).attr('href');
+                        });
+                    });
+                </script>
+            @endif
+
+            <script>
+                $(function () {
+                    $('[data-toggle="tooltip"]').tooltip();
+                })
+            </script>
+        @show
+    </body>
+</html>
+EOF
+
+# 3. Bersihkan Cache View
+php artisan view:clear
+
+echo "✅ Tampilan Sidebar berhasil di-update!"
+echo "🚫 Menu Settings, Locations, Nodes, Mounts, Nests sekarang TERSEMBUNYI untuk selain ID 1."
